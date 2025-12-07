96f6ecd0707f97dbbac3e81bb65b5455f777558e
thekcr85 <thekcr85@gmail.com>
2025-12-07 17:52:47 +0100

    Implement BookRepository in Infrastructure

---
+++ LibraryApi.Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,57 @@
+﻿using LibraryApi.Application.Interfaces.Repositories;
+using LibraryApi.Domain.Entities;
+
+namespace LibraryApi.Infrastructure.Repositories;
+
+public class BookRepository : IBookRepository
+{
+	public Task<IReadOnlyList<Book>> GetAllAsync()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Book?> GetByIdAsync(int id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Book> AddAsync(Book book)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Book> UpdateAsync(Book book)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<bool> DeleteAsync(int id)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Book?> GetByIsbnAsync(string isbn)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IReadOnlyList<Book>> GetByAuthorAsync(int authorId)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IReadOnlyList<Book>> SearchByTitleAsync(string titleFragment)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IReadOnlyList<Book>> GetByPublicationYearAsync(int year)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IReadOnlyList<Book>> GetRecent(int count)
+	{
+		throw new NotImplementedException();
+	}
+}
