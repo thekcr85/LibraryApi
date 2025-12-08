ddf09e0387e42c5ef6431dbae5d2bac7247e81c5
thekcr85 <thekcr85@gmail.com>
2025-12-08 15:03:21 +0100

    feat: Add BooksController and GetAllBooks() method

---
+++ LibraryApi.Api/Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using LibraryApi.Application.Books.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApi.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BooksController(IBookService bookService) : ControllerBase
+	{
+		[HttpGet]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		public async Task<IActionResult> GetAllBooks()
+		{
+			var books = await bookService.GetAllBooksAsync();
+			return Ok(books);
+		}
+	}
+}
