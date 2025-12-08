cc004e5f6485e28bcdaa82aaf78297ee7c02bcf8
thekcr85 <thekcr85@gmail.com>
2025-12-08 18:03:06 +0100

    feat: Add DI extension for Application services

---
+++ LibraryApi.Application/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,23 @@
+using LibraryApi.Application.Books.Interfaces.Services;
+using LibraryApi.Application.Books.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibraryApi.Application.DependencyInjection;
+
+/// <summary>
+/// Extension methods for configuring Application layer services.
+/// </summary>
+public static class DependencyInjection
+{
+	/// <summary>
+	/// Registers Application layer services to the dependency injection container.
+	/// </summary>
+	/// <param name="services">The service collection.</param>
+	/// <returns>The service collection for chaining.</returns>
+	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+	{
+		services.AddScoped<IBookService, BookService>();
+
+		return services;
+	}
+}
