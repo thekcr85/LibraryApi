be61077a91b88a68bbd51d2721f8eb869f637008
thekcr85 <thekcr85@gmail.com>
2025-12-08 15:20:57 +0100

    docs: Add XML documentation with <inheritdoc /> tags

--- LibraryApi.Application/Books/Services/BookService.cs
+++ LibraryApi.Application/Books/Services/BookService.cs
@@ -2,61 +2,78 @@
 using LibraryApi.Application.Books.Interfaces.Repositories;
 using LibraryApi.Application.Books.Interfaces.Services;
 using LibraryApi.Application.Books.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace LibraryApi.Application.Books.Services
 {
+	/// <summary>
+	/// Provides services for managing books
+	/// </summary>
 	public class BookService(IBookRepository bookRepository) : IBookService
 	{
+		/// <inheritdoc/>
 		public async Task<IReadOnlyList<BookDto>> GetAllBooksAsync()
 		{
 			var books = await bookRepository.GetAllAsync();
-			if (books == null || books.Count == 0)
-			{
-				return new List<BookDto>();
-			}
+			if (books is null || books.Count == 0)
+				return Array.Empty<BookDto>();
+
 			return books.Select(m => m.ToDto()).ToList();
 		}
 
-		public Task<BookDto?> GetBookByIdAsync(int id)
+		/// <inheritdoc/>
+		public async Task<BookDto?> GetBookByIdAsync(int id)
 		{
-			throw new NotImplementedException();
+			var book = await bookRepository.GetByIdAsync(id);
+			return book?.ToDto();
 		}
 
+		/// <inheritdoc/>
 		public Task<BookDto> CreateBookAsync(CreateBookDto createBookDto)
 		{
 			throw new NotImplementedException();
 		}
 
+		/// <inheritdoc/>
 		public Task<BookDto> UpdateBookAsync(int id, UpdateBookDto updateBookDto)
 		{
 			throw new NotImplementedException();
 		}
 
+		/// <inheritdoc/>
 		public Task<bool> DeleteBookAsync(int id)
 		{
 			throw new NotImplementedException();
 		}
 
+		/// <inheritdoc/>
 		public Task<BookDto?> GetBookByIsbnAsync(string isbn)
 		{
 			throw new NotImplementedException();
 		}
 
+		/// <inheritdoc/>
 		public Task<IReadOnlyList<BookDto>> GetBooksByAuthorAsync(int authorId)
 		{
 			throw new NotImplementedException();
 		}
 
+		/// <inheritdoc/>
 		public Task<IReadOnlyList<BookDto>> SearchBooksByTitleAsync(string titleFragment)
 		{
 			throw new NotImplementedException();
 		}
 
+		/// <inheritdoc/>
 		public Task<IReadOnlyList<BookDto>> GetBooksByPublicationYearAsync(int year)
 		{
 			throw new NotImplementedException();
 		}
 
+		/// <inheritdoc/>
 		public Task<IReadOnlyList<BookDto>> GetRecentBooksAsync(int count)
 		{
 			throw new NotImplementedException();
