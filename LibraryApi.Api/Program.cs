648a66bd7d2e88cdceb7e8f087577daea56b45b4
thekcr85 <thekcr85@gmail.com>
2025-12-09 16:32:24 +0100

    refactor: Clean up Program.cs configuration

--- LibraryApi.Api/Program.cs
+++ LibraryApi.Api/Program.cs
@@ -5,39 +5,15 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
 builder.Services.AddControllers();
-
-// Add modern ProblemDetails support (RFC 7807)
-builder.Services.AddProblemDetails(options =>
-{
-	options.CustomizeProblemDetails = ctx =>
-	{
-		ctx.ProblemDetails.Instance = ctx.HttpContext.Request.Path;
-		if (!ctx.HttpContext.Response.Headers.ContainsKey("X-Trace-Id"))
-		{
-			ctx.HttpContext.Response.Headers.Append("X-Trace-Id", ctx.HttpContext.TraceIdentifier);
-		}
-	};
-});
-
-// Add OpenAPI support
 builder.Services.AddOpenApi();
-
-// Add Infrastructure and Application layers
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
-// Exception handling pipeline (order matters!)
-app.UseExceptionHandler();
-app.UseStatusCodePages();
-
-// Custom exception handler middleware for domain-specific exceptions
 app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
 
-// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
 	app.MapOpenApi();
